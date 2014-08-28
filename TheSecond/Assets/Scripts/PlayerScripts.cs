3722e715e5d31e9763c36eeace935bfba84a8715
laminhtam2014 <minhtam.khtn.cntt@gmail.com>
2014-08-28 18:39:14 +0700

    + Add Second tutorial

---
+++ TheSecond/Assets/Scripts/PlayerScripts.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+public class PlayerScripts : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement * speed;
+		//keep player alway in the screen.
+		rigidbody.position = new Vector3(
+			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+		);
+		//end
+
+		// rotate when player turn left - right.
+		rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+		//end
+
+	}
+}
